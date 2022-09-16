4a2a553a17214ffb5c1962d856d74725a5ccde8b
Arshvir Singh <arshvirsignh16@gmail.com>
2022-09-16 08:04:10 -0700

    Add successful code to show nth row of pascal tri

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
-const ptRow = (n) => {
+function ptRow(n){
     if(n < 0){
-        return error;
+        return "Invalid Value";
     }
-    if(n = 0){
-        return [[1]];
+    if(n == 0){
+        return [1];
     }
+
+        let pyramid = [];
+    for(let row = 0; row < n; row++){
+        let rowList = [];
+        for(let column = 0; column <= row; column++){
+            if(column == 0 || column == row) {
+                rowList.push(1);
+            }
+            else{
+                let topLeft = pyramid[row - 1][column - 1];
+                let topRight = pyramid[row - 1][column];
+                let newValue = topLeft + topRight;
+                rowList.push(newValue);
+            }
+            
+            pyramid[row] = rowList;
+        }
+        
+
+    }
+    return pyramid[n - 1];
 }
+
+
+
+console.log(ptRow(15));
+
 
