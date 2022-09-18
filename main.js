7b8936c75db51a37b7f95a5aa621167eddaef26b
Arshvir Singh <arshvirsignh16@gmail.com>
2022-09-17 18:34:23 -0700

    Add comments to code.

--- main.js
+++ main.js
@@ -6,30 +6,30 @@ function ptRow(n){
         return [1];
     }
 
-        let pyramid = [];
+        let pyramid = []; //initializing main array(triangle)
     for(let row = 0; row < n; row++){
-        let rowList = [];
-        for(let column = 0; column <= row; column++){
-            if(column == 0 || column == row) {
+        let rowList = [];//each time the row variable implements, it will create a new row
+        for(let column = 0; column <= row; column++){ //create a "column" for each row depending on the row count
+            if(column == 0 || column == row) { //will add a 1 to the first and last column
                 rowList.push(1);
             }
             else{
-                let topLeft = pyramid[row - 1][column - 1];
-                let topRight = pyramid[row - 1][column];
-                let newValue = topLeft + topRight;
-                rowList.push(newValue);
+                let topLeft = pyramid[row - 1][column - 1]; //finds the number that is above(to the left) of the wanted value
+                let topRight = pyramid[row - 1][column]; //finds the number that is above(to the right) of the wanted value
+                let newValue = topLeft + topRight; //adds the top left/right to form the new value
+                rowList.push(newValue);//adds the new value
             }
             
-            pyramid[row] = rowList;
+            pyramid[row] = rowList; //adds the array to pyramid
         }
         
 
     }
-    return pyramid[n - 1];
+    return pyramid[n - 1];// bc index for the traingle starts at 0, have to return n-1 row
 }
 
 
 
-console.log(ptRow(15));
+// console.log(ptRow(3));
 
 
